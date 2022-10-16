3e791fa4fe15fb3ad36a523c517b52257d3c20e0
Mihail Mencyk <m.mencyk@gmail.com>
2022-10-16 23:47:40 +0300

    Четное число

--- EX06/Program.cs
+++ EX06/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = num % 2;
+if (x == 0) Console.WriteLine($"Число {num} четное");
+else Console.WriteLine($"Число {num} нечетное");

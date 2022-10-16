0c41b5aba5d92cb2c9dbfab69df562676ddbaf80
Mihail Mencyk <m.mencyk@gmail.com>
2022-10-17 00:32:44 +0300

    Четные числа от числа

---
+++ EX08/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = 2;
+Console.Write($"Четные числа от {num}: ");
+while (num > x)
+{
+    Console.Write($"{x} ");
+    x = x+2;
+}
+Console.WriteLine();
